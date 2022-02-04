08e9b4722e1e70cf5685b11cc76236b3d789cd94
isaacwms <isaac.wms17@gmail.com>
2022-02-04 01:08:17 -0500

    Exercise-02b

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,7 +6,8 @@ var rotation_speed = 5.0
 var speed = 0.1
 var max_speed = 10
 
-
+onready var Bullet = load("res://Player/Bullet.tscn")
+var nose = Vector2(0,-60)
 
 func _ready():
 	pass
@@ -15,6 +16,9 @@ func _physics_process(_delta):
 	position = position + velocity
 	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
 	
+	
+func get_input():
+	var to_return = Vector2.ZERO
 	$Exhaust.hide()
 	if Input.is_action_pressed("forward"):
 		velocity = velocity + Vector2(0,-speed).rotated(rotation)
@@ -23,6 +27,12 @@ func _physics_process(_delta):
 		rotation_degrees = rotation_degrees - rotation_speed
 	if Input.is_action_pressed("right"):
 		rotation_degrees = rotation_degrees + rotation_speed
-		
+	if Input.is_action_pressed("shoot"):
+		var Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var bullet = Bullet.instance()
+			bullet.global_position = global_position
+			bullet.rotation = rotation
+			Effects.add_child(bullet)
 	position.x = wrapf(position.x, 0, 1024)
 	position.y = wrapf(position.y, 0, 600)
